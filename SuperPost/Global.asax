efa019f442bcac5481e1ff2532875013f4e2fe1e
DanTheMinotaur <x15004091@student.ncirl.ie>
2017-12-11 21:32:33 +0000

    Initial

---
+++ SuperPost/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuperPost.MvcApplication" Language="C#" %>
---
+++ SuperPost/Models/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperPost.Models
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+    }
+
+    public class Comment
+    {
+        public int ID { get; set; }
+        public string CommentText { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
