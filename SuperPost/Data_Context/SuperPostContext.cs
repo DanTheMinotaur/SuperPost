35174786ff10e4822f552fecf36c7c21f7193c01
DanTheMinotaur <x15004091@student.ncirl.ie>
2017-12-11 21:36:23 +0000

    Added DB Context

---
+++ SuperPost/Data_Context/SuperPostContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SuperPost.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace SuperPost.Data_Context
+{
+    public class SuperPostContext : DbContext
+    {
+        public SuperPostContext() : base("SuperPostContext")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
