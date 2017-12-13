b4108de8cf8c14fd6b90fc0e93b2654f4fe38a89
DanTheMinotaur <x15004091@student.ncirl.ie>
2017-12-13 00:19:06 +0000

    Deleted Configuration

--- SuperPost/Migrations/Configuration.cs
+++ SuperPost/Migrations/Configuration.cs
@@ -5,12 +5,4 @@ namespace SuperPost.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<SuperPost.Data_Context.SuperPostContext>
-    {
-        public Configuration()
-        {
-            AutomaticMigrationsEnabled = false;
-            ContextKey = "SuperPost.Data_Context.SuperPostContext";
-        }
-    }
 }
