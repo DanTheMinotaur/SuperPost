eeed7adb37424e18f489ea78c82f52d282eb8e2c
DanTheMinotaur <x15004091@student.ncirl.ie>
2017-12-13 00:18:29 +0000

    Deleted Configuration

--- SuperPost/Migrations/Configuration.cs
+++ SuperPost/Migrations/Configuration.cs
@@ -12,21 +12,5 @@ namespace SuperPost.Migrations
             AutomaticMigrationsEnabled = false;
             ContextKey = "SuperPost.Data_Context.SuperPostContext";
         }
-
-        protected override void Seed(SuperPost.Data_Context.SuperPostContext context)
-        {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-        }
     }
 }
