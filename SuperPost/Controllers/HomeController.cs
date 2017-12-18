7eb7770524278c8de1a865905a2b2615cb896da6
DanTheMinotaur <x15004091@student.ncirl.ie>
2017-12-18 23:35:14 +0000

    Loading From POST Controller with Interface

--- SuperPost/Controllers/HomeController.cs
+++ SuperPost/Controllers/HomeController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SuperPost.DataContext;
+using SuperPost.Models;
+
 
 namespace SuperPost.Controllers
 {
     public class HomeController : Controller
     {
+        private SPContext db = new SPContext();
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.Posts.ToList());
         }
 
         public ActionResult About()
