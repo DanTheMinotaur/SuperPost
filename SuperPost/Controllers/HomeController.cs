6c21edb0e17ffb2493efd4b9bd71b195e610d9e5
DanTheMinotaur <x15004091@student.ncirl.ie>
2017-12-19 23:24:15 +0000

    Fixed Home Controller

--- SuperPost/Controllers/HomeController.cs
+++ SuperPost/Controllers/HomeController.cs
@@ -17,5 +17,19 @@ namespace SuperPost.Controllers
         {
             return View(db.Posts.ToList());
         }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
